44d0e53b0b75265e61d66333bc87f685443a0ad6
rafhtech <19240687@bsi.ac.id>
2025-04-25 08:43:31 +0700

    menerjakan tugas

---
+++ tugas02.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tugas Ke 2</title>
+    </head>
+    <body>
+        
+        <?php
+        // konstanta judul
+        define ("judul","Tugas 2 Pertemuan 3 Web Programing I");
+
+        // variabel data
+        $nama = "lintang Seruni Senja";
+        $alamat = "Jl. Badak No. 2 Cimahi-Bandung";
+        $ttl = "Jakarta, 20 Oktober 1990";
+        $tlp = "08212345678";
+
+        // mencetak data
+        echo judul;
+        echo "Nama Saya : $nama <br>";
+        echo "Alamat : $alamat <br>";
+        echo "TTL : $ttl <br>";
+        echo "Telp : $tlp <br>";
+        ?>
+    </body>
+</html>
