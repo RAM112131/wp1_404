f6cbc965be8b8595ed44c05d3984dd22442dc2d0
rafhtech <19240687@bsi.ac.id>
2025-05-02 05:24:52 +0700

    operator aritmatika

---
+++ operatoraritmatika.php
@@ -0,0 +1,18 @@
+<?php
+
+// variabel
+$bil1 = 200;
+$bil2 = 40;
+
+$hasil_jumlah = $bil1 + $bil2;
+$hasil_kurang = $bil1 - $bil2;
+$hasil_kali = $bil1 * $bil2;
+$hasil_bagi = $bil1 / $bil2;
+
+echo 
+"Ini adalah latihan operator aritmatika<br>
+$bil1 + $bil2 = $hasil_jumlah<br>
+$bil1 - $bil2 = $hasil_kurang<br>
+$bil1 * $bil2 = $hasil_kali<br>
+$bil1 / $bil2 = $hasil_bagi";
+?>
