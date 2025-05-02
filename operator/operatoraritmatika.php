0a8cb244a92b380914a6a7e7301412dea81eff80
rafhtech <19240687@bsi.ac.id>
2025-05-02 09:57:25 +0700

    update materi

--- operator/operatoraritmatika.php
+++ operator/operatoraritmatika.php
@@ -8,11 +8,27 @@ $hasil_jumlah = $bil1 + $bil2;
 $hasil_kurang = $bil1 - $bil2;
 $hasil_kali = $bil1 * $bil2;
 $hasil_bagi = $bil1 / $bil2;
+$hasil_hasilbagi = $bil1 % $bil2;
 
 echo 
 "Ini adalah latihan operator aritmatika<br>
+penjumlahan : <br>
 $bil1 + $bil2 = $hasil_jumlah<br>
+pengurangan :<br>
 $bil1 - $bil2 = $hasil_kurang<br>
+perkalian : <br>
 $bil1 * $bil2 = $hasil_kali<br>
-$bil1 / $bil2 = $hasil_bagi";
+pembagian : <br>
+$bil1 / $bil2 = $hasil_bagi<br>
+hasil bagi : <br>
+$bil1 % $bil2 = $hasil_hasilbagi<br>
+";
+
+$angka1 = 22;
+$angka2 = 7;
+$hasilbagi = $angka1 / $angka2;
+
+echo 
+"pembagian dengan mengambil sebagian angka dibelakang koma<br>
+hasil dari $angka1 / $angka2 = ".round($hasilbagi, 4)."<br>";
 ?>
--- operator/operatorperbandingan.php
+++ operator/operatorperbandingan.php
@@ -5,22 +5,26 @@ $teks1="PHP";
 $teks2="php";
 
 $hasil = $bil1 == $bil2;
-echo "$bil1 == $bil2 = $hasil<br>";
+echo "$bil1 == $bil2 = $hasil | ",var_dump($bil1 == $bil2),"<br>";
+
 // keterangan :
 // jika hasilnya ()/kosong, maka itu false, jika hasilnya 1 maka hasilnya true
 
 $hasil = $bil1 != $bil2;
-echo "$bil1 == $bil2 = $hasil<br>";
+echo "$bil1 != $bil2 = $hasil | ",var_dump($bil1 != $bil2),"<br>";
 
 $hasil = $bil1 >= $bil2;
-echo "$bil1 >= $bil2 = $hasil<br>";
+echo "$bil1 >= $bil2 = $hasil | ",var_dump($bil1 >= $bil2),"<br>";
+
+$hasil = $bil1 <= $bil2;
+echo "$bil1 <= $bil2 = $hasil | ",var_dump($bil1 <= $bil2),"<br>";
+
 
 $hasil = $teks1 == $teks2;
-echo "$teks1 == $teks2 = $hasil<br>";
-
+echo "$teks1 == $teks2 = $hasil | ",var_dump($teks1 == $teks2),"<br>";
 
 $hasil2 = $teks1 != $teks2;
-echo "$teks1 != $teks2 = $hasil2";
+echo "$teks1 != $teks2 = $hasil | ",var_dump($teks1 != $teks2),"<br>";
 
 
 ?>
