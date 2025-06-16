9ccd289b47ed26eaee915686d62009cf19f12a6d
rafhtech <19240687@bsi.ac.id>
2025-06-16 08:19:56 +0700

    update script

--- tugas/tugas10/hasil_daging_kurban.php
+++ tugas/tugas10/hasil_daging_kurban.php
@@ -3,14 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Hasil Pembagian Daging Kurban</title>
 </head>
-<body>
-    <?php
+<body style="line-height: 1.5;">
+    <div style="justify-content: center; display: flex;"> 
+        <fieldset>
+            <?php
     $daging_bersih = 65;
     $jumlah_sapi = $_POST["jumlah_sapi"];
     $total_daging = $jumlah_sapi * $daging_bersih;
     $nama = explode("\n", $_POST["daftar_nama"]);
+    $nama = array_filter($nama, function($value) {
+        return trim($value) !== '';
+    });
+
     $jumlah_warga = count($nama);
     $jatah_daging = $total_daging / $jumlah_warga;
 
@@ -18,10 +24,27 @@
     echo "Jumlah Warga : $jumlah_warga <br>";
     echo "Jatah Daging Kurban : $jatah_daging Kg<br><br>";
     
+    echo "<table border='1' cellpadding='6' cellspacing='0' style='border-collapse: collapse;'>
+    <tr style='background-color: #00BFFF'>
+        <th>Nomor Antrian</th>
+        <th>Nama Warga</th>
+        <th>Jatah Daging (Kg)</th>
+    </tr>
+    ";
     for ($i = 0; $i < count($nama); $i++) {
-        echo $nama[$i] . "<br>";
+    $antrian = $i + 1;
+    echo "<tr>
+        <td>$antrian</td>
+        <td>$nama[$i]</td>
+        <td>$jatah_daging</td>
+    </tr>";
     }
+
+    echo"</table><br>";
     ?>
+        </fieldset>
+    </div>
+
 
 </body>
 </html>
--- tugas/tugas10/input.php
+++ tugas/tugas10/input.php
@@ -3,24 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Inputan daging</title>
 </head>
 <body>
-    <h1>Pembagian Daging Kurban</h1>
-    <form method="POST" action="hasil_daging_kurban.php">
+    <div style="justify-content: center; display: flex; line-height: 1.5;">
+        <form method="POST" action="hasil_daging_kurban.php">
         <fieldset>
+            <h2 >Pembagian Daging Kurban</h2>
             <p>
-                <label for>Jumlah Sapi Kurban : </label><br>
-                <input type="number" name="jumlah_sapi" required min="1"></td>
+                <label >Jumlah Sapi Kurban : </label><br>
+                <input type="number" name="jumlah_sapi" required min="1" style="width: 270px;"></td>
             </p>
             <p>
-                <label for>Daftar Nama Warga (satu nama per baris) : </label><br>
-                <textarea name="daftar_nama" cols="30" rows="10" required></textarea>
+                <label >Daftar Nama Warga (satu nama per baris) : </label><br>
+                <textarea name="daftar_nama" cols="35" rows="10" required></textarea>
             </p>
-            <p>
+            <p style="text-align: center;">
                 <input type="submit" value="Hitung dan Tampilkan Jatah">
             </p>
         </fieldset>
-    </form>
+        </form>
+    </div>
+
+<script type="text/javascript">
+    document.querySelector('form').addEventListener('submit', function(event) {
+        event.preventDefault(); 
+        alert('Data berhasil diproses!');
+        this.submit();
+    });
+</script>
 </body>
 </html>
+
